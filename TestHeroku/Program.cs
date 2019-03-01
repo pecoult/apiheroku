26bbd3d9ec47a9bf885cf90dc314199923b72a26
PECOULT Romain <romain.pecoult@orange.com>
2019-03-01 13:13:00 +0100

    Init repo with simple api

---
+++ TestHeroku/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TestHeroku
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+                    WebHost.CreateDefaultBuilder(args)
+                        .UseStartup<Startup>()
+                         .UseKestrel((context, options) =>
+                         {
+                             var port = Environment.GetEnvironmentVariable("PORT");
+                             if (!string.IsNullOrEmpty(port))
+                             {
+                                 options.ListenAnyIP(int.Parse(port));
+                             }
+                         });
+    }
+}
